b946b9c473b8c5fb6fd0fb1062fefdadabf41794
Roach <43852412+RoachyT@users.noreply.github.com>
2018-10-12 16:18:44 -0400

    Add files via upload

---
+++ Bond_James/Bond_James_Toles/Bond_James_Toles/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bond_James_Toles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your First name?");
+            var firstName = Console.ReadLine();
+            Console.WriteLine("What is your Last name?");
+            var lastName = Console.ReadLine();
+
+            Console.WriteLine(lastName + " " + firstName);
+            Console.ReadKey();
+        }
+    }
+}
